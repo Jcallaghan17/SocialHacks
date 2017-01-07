b82509035cd03607d142f09a9b7dd9045dcdee91
Billy Wong <bwong5@stuy.edu>
2017-01-07 17:07:23 -0500

    facebook progress

--- utils/facebookEvents.py
+++ utils/facebookEvents.py
@@ -1,20 +1,32 @@
 import facebook
 
-graph = facebook.GraphAPI(access_token='EAARsOJeuDDIBANtqRaYZBlnB6ex4IZCKJUSzi27PvueRzMisYNYsFO68w52xM6U3UnHOfbd4wGYYJYH0nKzFZA7e8qtVLjJBuI9NPKDVbhF68pfmQLeL6RgABsStSApb7L1aveXXsM8upC921Wrd26JBlFAZAHBIpaEgvW8bXgZDZD')
 
-post = graph.get_object(id='post_id')
-print(post['message'])
+#make sure to:
+#    pip install -e git+https://github.com/mobolic/facebook-sdk.git#egg=facebook-sdk
 
-#event = graph.get_object(id='event_id', fields='attending_count,declined_count')
-#print(event['attending_count'])
-#print(event['declined_count'])
+#Documentation:
+#https://developers.facebook.com/docs/graph-api/reference/event
+#https://developers.facebook.com/docs/graph-api/reference/v2.8/group/events
+#https://facebook-sdk.readthedocs.io/en/latest/api.html
 
 
 
-'''
-graph = facebook.GraphAPI(oauth_access_token)
-profile = graph.get_object("me")
-friends = graph.get_connections("me", "friends")
-graph.put_object("me", "feed", message="I am writing on my wall!")
 
-'''
+#change this: user token
+#https://developers.facebook.com/tools/access_token/
+
+token = 'EAAMZAuGqTFEcBAAAXFbEaugyb4ZAZClgwOWRKtltsdZAmjmeWiY0GHZCq8nB7olIZAFRZBlNYpipPNZBCKzuLJtU2V2cQjl8gRni8AMP4wvHjnwI8AxTuqbVZBZCVN3WT05VQa88fVUccZBbNuprlbUlv78QjZBueT22o8CpUDm51CxGZCQZDZD'
+
+graph = facebook.GraphAPI(token)
+
+
+event = graph.get_object(id='706776926156529', fields='description,start_time,end_time,place')
+print(event['description'])
+print(event['start_time'])
+print(event['end_time']
+print(event['place'])
+
+#not tested
+page = graph.get_object(id='358192317896345/events')
+for thing in page:
+    print thing
