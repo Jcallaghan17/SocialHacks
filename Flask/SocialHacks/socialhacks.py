71e7094bf5deb4479a322fefc18389c758ba815f
Joe Callaghan <jcallaghan17@regis.org>
2017-01-07 16:44:42 -0500

    Schema and SocialHacks
    Updated SocialHacks.py and Schema.sql
    added Style.css
    added layout.html
    I still don't know flask

--- Flask/SocialHacks/socialhacks.py
+++ Flask/SocialHacks/socialhacks.py
@@ -10,8 +10,14 @@ app.config.from_object(__name__) # load config from this file
 # Load default config and override config from an environment variable
 app.config.update(dict(
     DATABASE=os.path.join(app.root_path, 'socialhacks.db'),
-    SECRET_KEY='development key',
+    SECRET_KEY='development_key',
     USERNAME='admin',
     PASSWORD='default'
 ))
 app.config.from_envvar('SOCIALHACKS_SETTINGS', silent=True)
+
+def connect_db():
+    """Connects to the specific database."""
+    rv = sqlite3.connect(app.config['DATABASE'])
+    rv.row_factory = sqlite3.Row
+    return rv
