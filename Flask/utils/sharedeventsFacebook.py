2a8d885b16e0b9fe0574f0ff367c0a0047a92334
Joe Callaghan <jcallaghan17@regis.org>
2017-01-08 01:55:42 -0500

    Shared Events
    Gets object_id which equates to the actual event id

--- Flask/utils/sharedeventsFacebook.py
+++ Flask/utils/sharedeventsFacebook.py
@@ -1,19 +1,31 @@
 import facebook
 import json
 import addEventToDB
+import facebookEvents
 token = 'EAAC1mYYLrqkBAFVX4KHqZBhf9ZBTwOKtyr2x7NDP59iSkh9DiqOgXMU9xGCed2gURoBAPiLWDO7gpxNmMPPLphAmskuRCJtuvCO9w6taHZBSomoWZA8RMvXT61uytQl4ORHsogURgjMYkwHK20g78Df4ZAZCNxrk0ZD'
 graph = facebook.GraphAPI(token)
 GROUPID = 358192317896345
 
-feed = graph.get_object(id=str(GROUPID)+'/feed')
+feed = graph.get_object(id=str(GROUPID)+'/feed', fields ='id, object_id')
 feed_json = json.dumps(feed)
 feed_dict = json.loads(feed_json)
 length = len(feed_dict['data'])
 i = 0
+def get_object_id(i): #Not to be confused with event id
+#This gets the object id which only appears in the feed if its an event
+	print feed_dict['data'][i]['object_id']
+	return feed_dict['data'][i]['object_id'] #This should give a valid event id
+
+
+
+get_object_id():
+
+
+'''
 while (i < length):
 	print feed_dict['data'][i]
 	i+=1
-	
+'''
 #TODO
 #GO THROUGH FEED
 #FIND SHARED EVENTS
